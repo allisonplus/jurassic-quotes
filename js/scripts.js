a8896d08bcbd79906606795309289db8aff485b5
Allison Tarr <hello@allisontarr.com>
2015-05-25 13:22:16 -0400

    quote functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
+var jpq = {};
+
+jpq.quotes = ["When you gotta go, you gotta go", "Clever girl", "Dino DNA", "Hello, John"];
+
+//Function that loads first quote on page load
 $(document).ready(function() {
-
-	var quotes = ["When you gotta go, you gotta go", "Clever girl", "Dino DNA", "Hello, John"];
-
-
-
+	$("#quote").html(jpq.quotes[jpq.random()]);
 }); //END DOCUMENT READY
+
+//Randomize quote picker
+jpq.random = function() {
+	return Math.floor(Math.random()*(jpq.quotes.length));
+}
+
+//On button click, fade out current quote and fade in next quote
+$(function() {
+	$(".button").on('click', function() {
+		$("#quote").fadeOut(function() {
+			$("#quote").fadeIn().html(jpq.quotes[jpq.random()]);
+		});
+	});
+});
+
