09901451d8eaf78351fcbe6db43faa47a31ea0ad
Allison Tarr <hello@allisontarr.com>
2015-05-25 15:37:12 -0400

    added more quotes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 var jpq = {};
 
-jpq.quotes = ["When you gotta go, you gotta go", "Clever girl", "Dino DNA", "Hello, John"];
+jpq.quotes = [
+	"When you gotta go, you gotta go",
+	"Hold onto your butts.",
+	"God help us, we're in the hands of engineers.",
+	"Gee, the lack of humility before nature that's being displayed here, uh... staggers me.",
+	"Find Nedry! Check the vending machines!",
+	"We spared no expense.",
+	"Dodgson, Dodgson, we've got Dodgson here! Nobody cares.",
+	"It's a UNIX system! I know this!",
+	"Look... We can discuss sexism in survival situations when I get back.",
+	"I am not a computer nerd. I prefer to be called a hacker!",
+	"Clever girl",
+	"Dino DNA",
+	"Hello, John"
+];
 
 //Function that loads first quote on page load
 $(document).ready(function() {
